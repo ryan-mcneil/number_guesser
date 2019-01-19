fc17aa00d948d2b147860db5698c178dc980d513
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-01-18 19:39:37 -0700

    user cna change range

--- script.js
+++ script.js
@@ -15,8 +15,7 @@ $(document).ready(function(){
   });
 
   $('#guess').click(function(){
-    const input = document.getElementById("input").value
-
+    const input = document.getElementById("input").value;
     if ( validate(input) ) {
       $('#last-note').text('Your last guess was');
       $('#last').text(input);
@@ -32,22 +31,39 @@ $(document).ready(function(){
   });
 
   $('#clear').click(function(){
-    $('input:text').val("");
-  })
+    clear();
+  });
 
   $('#reset').click(function(){
     reset();
-  })
+  });
+
+  $('#update').click(function(){
+    let newMin = parseInt(document.getElementById("update-min").value);
+    let newMax = parseInt(document.getElementById("update-max").value);
+
+    if (validateMin(newMin, newMax) && validateMax(newMin, newMax)) {
+      min = newMin;
+      max = newMax;
+      $('#min').text(min);
+      $('#max').text(max);
+      reset();
+    }
+  });
 
   function randomNumber() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  function reset () {
+  function reset() {
     $('#last').text("");
     $('.feedback-text').text("");
-    $('input:text').val("");
     actual = randomNumber();
+    clear();
+  }
+
+  function clear() {
+    $('input:text').val("");
   }
 
   function validate(input) {
@@ -75,6 +91,23 @@ $(document).ready(function(){
     }
   }
 
+  function validateMin(newMin, newMax) {
+    if (isNaN(parseInt(newMin)) || newMin > newMax) {
+      alert('Min should be an integer less than the Max');
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function validateMax(newMin, newMax) {
+    if (isNaN(parseInt(newMax)) || newMax < newMin) {
+      alert('Max should be an integer greater than the Min');
+      return false;
+    } else {
+      return true;
+    }
+  }
 
 
 });
